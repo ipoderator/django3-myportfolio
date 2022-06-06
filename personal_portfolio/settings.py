9b7687c48754fd19eb3e10f84d3192505d9070fa
Gleb Churkin <gleb.churkin48@gmail.com>
2022-06-06 20:47:38 +0300

    commit

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -15,6 +15,20 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+import os
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+SECRET_KEY = 'hf&y$+n4mis@*@vxu6!np(ertasdadasdfsf??&Dfs#omh4aih%wlz-q&ys%$zu!e)o'
+
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['127.0.0.1']
+
+
 
 # Application definition
 
@@ -64,6 +78,17 @@ WSGI_APPLICATION = 'personal_portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'portfolio',
+        'USER': 'userdb',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -104,7 +129,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_DIR = os.path.join(BASE_DIR, 'static/')
+STATIC_DIRS = [STATIC_DIR]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 try:
